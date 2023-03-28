cb8d3adbe883aaa37493c59592f28f98ed6e4ef1
jepbarasgarov <asgarovjepbar00@gmail.com>
2023-03-28 18:44:22 +0500

    second

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const server = http.createServer((req, res) => {
     res.write('OK');
     res.end();
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.write('404 Not Found');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('OK not health');
     res.end();
   }
 });
